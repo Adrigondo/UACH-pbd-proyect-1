149591ee5159a9990130fee2dee1b2da4e973eee
Adrigondo <adrigondo.profesional@gmail.com>
2024-04-06 18:32:22 -0600

    Update: secondary menu

--- manual.sh
+++ manual.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-selectedOpt=""
+selectedOption=""
 selectedSection=""
 selectedSectionName=""
+selectedOperation=""
+currentMenu=""
 
-while getopts ":at" selectedOpt; do
-  case ${selectedOpt} in
-    a )
+while getopts ":at" selectedOption; do
+  case ${selectedOption} in
+    'a' )
         echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:"
         echo "1) SCRUM"
         echo "2) X.P."
@@ -31,7 +33,7 @@ while getopts ":at" selectedOpt; do
           ;;
       esac
       ;;
-    t )
+    't' )
         echo "Bienvenido a la guía rápida de metodologías tradicionales, para continuar seleccione un tema:"
         echo "1) Cascada"
         echo "2) Espiral"
@@ -54,11 +56,66 @@ while getopts ":at" selectedOpt; do
       esac
       ;;
     \? )
-      echo "Opción inválida: -$OPTARG"
-      ;;
-    : )
-      echo "La opción -$OPTARG requiere un argumento."
+        echo "Opción inválida: -$OPTARG"
+        echo "  -a Revisar manual de Agile"
+        echo "  -t Revisar manual de metodologías tradicionales"
+        exit 2
       ;;
   esac
 done
-echo "La opción seleccionada fue ${selectedSectionName}"
+if [[ ${selectedOption} == '?' ]]; then
+  echo "Falta un argumento:"
+  echo "  -a Revisar manual de Agile"
+  echo "  -t Revisar manual de metodologías tradicionales"
+  exit 1
+fi
+
+
+echo "Usted esta en la sección ${selectedSectionName}, seleccione la opción que desea utilizar."
+echo "1) Agregar información"
+echo "2) Buscar"
+echo "3) Eliminar información"
+echo "4) Leer base de información."
+read -p "Seleccione una opción (1-4): " selectedOperation
+
+case $selectedOperation in
+1 )
+    echo "> Agregar información"
+    concept=""
+    definition=""
+    read -p "Ingresa el nombre del concepto: " concept
+    read -p "Ingresa la definición del concepto: " definition
+
+    # Todo: Save concept and definition into file.
+  ;;
+2 )
+    echo "> Buscar"
+    concept=""
+    read -p "Ingresa el concepto a buscar: " concept
+
+    # Todo
+    # Search concept using REGEX from the file with the selectedSectionName name
+    # If the concept doesn't exists display an error message.
+    # If the concept exists, display the definition of the concept
+  ;;
+3 )
+    echo "> Eliminar información"
+    concept=""
+    read -p "Ingresa el concepto que desea eliminar: " concept
+
+    # Todo
+    # Delete the concept from the file with the selectedSectionName name
+    # Check if the concept extists, if not, display an error message
+  ;;
+4 )
+    echo "> Leer base de información."
+
+    # Todo
+    # Show all the information of the file with the selectedSectionName name
+    # If the file doesn't exists or it is empty, display an error message
+  ;;
+* )
+    echo "Selección inválida. Por favor, seleccione un número del 1 al 3."
+  ;;
+
+
